b9e22581dddd9faa6beb5ab5e70c8dc71ce1249f
dirkarnez <smalldirkalex@gmail.com>
2024-10-28 21:29:34 +0800

    - wip

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const pdfjsLib = require("pdfjs-dist");
-
-const PAGE_NUMBER = 1;
-
-pdfjsLib
-.getDocument("ctn.pdf").promise
-.then(pdfDocument => pdfDocument.getPage(PAGE_NUMBER))
-.then(page => page.getTextContent())
-.then(textContent => console.log(textContent.items.map(item => item.str).join("")))
-.catch(e => {
-    console.error(e);
+var libclang = require('libclang');
+ 
+var index = new libclang.index();
+var tu = new libclang.translationunit();
+ 
+tu.fromSource(idx, 'myLibrary.h', ['-I/path/to/my/project']);
+ 
+tu.cursor().visitChildren(function (parent) {
+  switch (this.kind) {
+    case libclang.KINDS.CXCursor_FunctionDecl:
+      console.log(this.spelling);
+      break;
+  }
+  return libclang.CXChildVisit_Continue;
 });
+ 
+index.dispose();
+tu.dispose();
